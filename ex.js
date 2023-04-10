0c25d4843b645efcd8f28e79ec3f9ae38bb54d01
daicqcoder <daidtvtppg@gmail.com>
2023-04-10 15:36:43 +0700

    add express ex

--- ex.js
+++ ex.js
@@ -1,6 +1,11 @@
-const http = require("http");
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.write('Hi NodeJS!');
-    res.end();
-}).listen(port=8080);
+app.get('/',(req, res) =>{
+    res.send("Hello Express David Chu!")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
